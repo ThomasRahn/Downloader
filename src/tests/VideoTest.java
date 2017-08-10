a3d80e07f7e6d1a587a27784f42e63fcc6078931
Thomas Rahn <thomas@rahn.ca>
2017-08-09 21:28:57 -0400

    Started initial tests

--- src/tests/VideoTest.java
+++ src/tests/VideoTest.java
@@ -1,14 +1,16 @@
 package tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import file.Video;
 
 public class VideoTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCanCreateVideo() {
+		Video vid = new Video(1, "http://www.google.ca", "test.html");
+		
+		assertTrue(vid instanceof Video);
 	}
-
+	
 }
