ed4c15409a0770e66f0b90e5834c1f1c211a7489
Thomas Rahn <thomas@rahn.ca>
2017-08-17 22:28:02 -0400

    Forgot to add active record when set up Config class

--- src/relationModel/ActiveRecord.java
+++ src/relationModel/ActiveRecord.java
@@ -2,15 +2,19 @@ package relationModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import main.Config;
+import storage.Database;
 
 public class ActiveRecord {
 	
 	private List<ActiveField> fields = null;
 	private String table_name;  
+	private Database db;
 	
 	public ActiveRecord(String table_name) {
 		this.fields = new ArrayList<ActiveField>();
 		this.table_name = table_name;
+		db = Config.getDatabase();
 	}
 	
 	public void registerField(String name, Object obj){
@@ -38,11 +42,11 @@ public class ActiveRecord {
 	}
 	
 	public void create_structure(){
-		
+		db.createStructure(this);
 	}
 	
 	public void save(){
-		
+		db.save(this);
 	}
 
 	
